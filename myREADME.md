c93130ff0c7a18bc82eea7ea313b9eaffe0fb9ee
ArthurKa <arthur.katruk@gmail.com>
2021-09-19 00:56:09 +0300

    feat: implement main functionality

---
+++ myREADME.md
@@ -0,0 +1,38 @@
+## .env
+```
+MONGO_URL = mongodb://localhost:27017/<DB_NAME>
+```
+
+### Or
+```
+MONGO_URL = mongodb://localhost:27017
+DB_NAME = <DB_NAME>
+```
+
+## Usage
+
+```ts
+import { getCollection, closeDB } from '<pkg.name>';
+
+(async () => {
+  const collection = await getCollection('collection');
+
+  await closeDB();
+})();
+```
+
+### Or
+```ts
+import { getCollection, closeDB } from '<pkg.name>';
+
+interface Document {
+  name: string;
+  age: number;
+}
+
+(async () => {
+  const collection = await getCollection<Document>('collection');
+
+  await closeDB();
+})();
+```
---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import { MongoClient, Db, Collection, Document, OptionalId } from 'mongodb';
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const dbName = process.env.DB_NAME || process.env.MONGO_URL.split('/').pop()!;
+let data: {
+  client: MongoClient;
+  db: Db;
+} | null = null;
+const collections: Record<string, Collection<any>> = {};
+
+async function throwIfNoCollection(db: Db, collectionName: string) {
+  const collections = await db.listCollections().toArray().then(e => e.map(e => e.name));
+  if(!collections.includes(collectionName)) {
+    throw new Error(`There is no such collection as "${collectionName}" in ${dbName}`);
+  }
+}
+
+export async function getCollection<T extends Document = Document>(collectionName: string, createCollection?: true) {
+  if(!data) {
+    const client = await MongoClient.connect(process.env.MONGO_URL);
+    data = {
+      client,
+      db: client.db(dbName),
+    };
+  }
+
+  let collection: Collection<T> | undefined = collections[collectionName];
+  if(!collection) {
+    if(!createCollection) {
+      await throwIfNoCollection(data.db, collectionName);
+    }
+    collection = collections[collectionName] = data.db.collection<T>(collectionName);
+  }
+
+  if(createCollection) {
+    await collection.insertOne({} as OptionalId<T>);
+    await collection.deleteMany({});
+  }
+  return collection;
+}
+
+export const closeDB = () => {
+  const result = data?.client.close();
+  data = null;
+  return result;
+};
